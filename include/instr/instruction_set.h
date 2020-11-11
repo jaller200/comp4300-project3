9ec68bb135d8ccbeded28b30b11e07872f7d6b15
Jonathan Hart <jonathanhart3000@gmail.com>
2020-11-11 01:27:19 -0600

    Created some basic barebones headers

---
+++ include/instr/instruction_set.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/**
+ * A basic class that holds an instruction set.
+ */
+class InstructionSet {
+public:
+
+    // MARK: -- Construction
+    InstructionSet() = default;
+    ~InstructionSet() = default;
+};
